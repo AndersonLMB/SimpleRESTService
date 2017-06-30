e7d1aa47c324dd4c7b1d989495dbdd88b6033b51
AndersonLMB <1603497362@qq.com>
2017-06-30 11:47:13 +0800

    Commit

--- SimpleRESTService/Service1.svc.cs
+++ SimpleRESTService/Service1.svc.cs
@@ -8,26 +8,38 @@ using System.Text;
 
 namespace SimpleRESTService
 {
-    // 注意: 使用“重构”菜单上的“重命名”命令，可以同时更改代码、svc 和配置文件中的类名“Service1”。
-    // 注意: 为了启动 WCF 测试客户端以测试此服务，请在解决方案资源管理器中选择 Service1.svc 或 Service1.svc.cs，然后开始调试。
-    public class Service1 : IService1
+    //// 注意: 使用“重构”菜单上的“重命名”命令，可以同时更改代码、svc 和配置文件中的类名“Service1”。
+    //// 注意: 为了启动 WCF 测试客户端以测试此服务，请在解决方案资源管理器中选择 Service1.svc 或 Service1.svc.cs，然后开始调试。
+    //public class Service1 : IService1
+    //{
+    //    public string GetData(int value)
+    //    {
+    //        return string.Format("You entered: {0}", value);
+    //    }
+
+    //    public CompositeType GetDataUsingDataContract(CompositeType composite)
+    //    {
+    //        if (composite == null)
+    //        {
+    //            throw new ArgumentNullException("composite");
+    //        }
+    //        if (composite.BoolValue)
+    //        {
+    //            composite.StringValue += "Suffix";
+    //        }
+    //        return composite;
+    //    }
+    //}
+    public class Service1 : IService
     {
-        public string GetData(int value)
+        public string EchoWithGet(string s)
         {
-            return string.Format("You entered: {0}", value);
+            return "You said " + s;
         }
 
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        public string EchoWithPost(string s)
         {
-            if (composite == null)
-            {
-                throw new ArgumentNullException("composite");
-            }
-            if (composite.BoolValue)
-            {
-                composite.StringValue += "Suffix";
-            }
-            return composite;
+            return "You said " + s;
         }
     }
 }
