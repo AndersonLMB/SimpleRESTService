6d21f96cb3b17936d77f117ee9411e468251a458
AndersonLMB <1603497362@qq.com>
2017-07-03 11:34:33 +0800

    commit 20170703

--- EmployeeService/EmpInfoService.svc.cs
+++ EmployeeService/EmpInfoService.svc.cs
@@ -4,11 +4,16 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
+using System.ServiceModel.Activation;
+using Dapper;
+using MySql.Data;
+using MySql.Data.MySqlClient;
 
 namespace EmployeeService
 {
     // 注意: 使用“重构”菜单上的“重命名”命令，可以同时更改代码、svc 和配置文件中的类名“EmpInfoService”。
     // 注意: 为了启动 WCF 测试客户端以测试此服务，请在解决方案资源管理器中选择 EmpInfoService.svc 或 EmpInfoService.svc.cs，然后开始调试。
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
     public class EmpInfoService : IEmpInfoService
     {
         public string GetEmpSalary(string EmpId)
@@ -16,4 +21,55 @@ namespace EmployeeService
             return "Salary of " + EmpId + " is " + 2000;
         }
     }
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class UserInfoService : IUserInfoService
+    {
+        public UserInfoService()
+        {
+            refreshData();
+        }
+        private void refreshData()
+        {
+            MySqlConnection connenction = new MySqlConnection();
+            connenction.ConnectionString = "Server=localhost;Database=test;Uid=lmb;Pwd=1234;";
+            try
+            {
+                var list = connenction.Query<User>("select * from test.table1");
+                users = list.ToList();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+        List<User> users { set; get; }
+        public string GetInfo(string id, string type)
+        {
+            refreshData();
+
+            User user = new User();
+            MySqlConnection connenction = new MySqlConnection();
+            connenction.ConnectionString = "Server=localhost;Database=test;Uid=lmb;Pwd=1234;";
+            string sqlstring = "select " + type.ToString() + " from test.table1 where id=" + id.ToString();
+            try
+            {
+                user = connenction.QueryFirst<User>(sqlstring);
+                string str = user.GetType().GetProperty(type).GetValue(user).ToString();
+                return str;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return "";
+
+
+
+
+
+        }
+
+    }
+
+
 }
--- EmployeeService/IEmpInfoService.cs
+++ EmployeeService/IEmpInfoService.cs
@@ -5,10 +5,14 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
+using System.ServiceModel.Activation;
 
 namespace EmployeeService
 {
     // 注意: 使用“重构”菜单上的“重命名”命令，可以同时更改代码和配置文件中的接口名“IEmpInfoService”。
+
+ 
+
     [ServiceContract]
     public interface IEmpInfoService
     {
@@ -16,11 +20,37 @@ namespace EmployeeService
         [WebInvoke
          (
             Method = "GET",
-            UriTemplate = "/EmpSalaryDetail/{EmpId}",
+            UriTemplate = "EmpInfoDetail/{EmpId}",
             RequestFormat = WebMessageFormat.Json,
             ResponseFormat = WebMessageFormat.Json,
             BodyStyle = WebMessageBodyStyle.Wrapped
             )]
         string GetEmpSalary(string EmpId);
     }
+
+    [ServiceContract]
+    public interface IUserInfoService
+    {
+        [OperationContract]
+        [WebInvoke(
+            Method = "GET",
+            UriTemplate = "UserInfo/{id}/{type}",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Wrapped)]
+        string GetInfo(string id, string type);
+    }
+
+    [DataContract]
+    public class User
+    {
+        [DataMember]
+
+        public string id { get; set; }
+        [DataMember]
+        public string name { get; set; }
+        [DataMember]
+        public string password { get; set; }
+    }
+
 }
